84a6fe7a2b261faf4b3b5890e45583e11ff2cd28
iinnesperado <inestianrp@gmail.com>
2025-03-10 17:21:55 +0100

    aded tests

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from faster_whisper import WhisperModel
 import time
+import os
+import glob
 
 model_size = "large-v3"
 
@@ -10,13 +12,23 @@ model_size = "large-v3"
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 
-model = WhisperModel(model_size, device="cpu", compute_type="int8")
-start = time.time()
-segments, info = model.transcribe("samples/test.m4a", beam_size=5)
-end = time.time()
-print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-print("Transcription took %.2fs" % (end - start))
+path = "/Users/ines/androide/p-androide/samples"
 
-for segment in segments:
-    print("hello")
-    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+timeFile = open("exec_time.txt", "w")
+
+for name in glob.glob('samples/*.m4a'):
+    start = time.time()
+    segments, info = model.transcribe(name, beam_size=5)
+    end = time.time()
+    #print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+    #print("Transcription took %.2fs" % (end - start))
+
+    timeFile.write(f"{name}\t")
+    timeFile.write("%.2f\n" % (end - start))
+
+    print("\n---")
+    print("Transcription of %s" % (name))
+    for segment in segments:
+        print("%s" % (segment.text))
+timeFile.close()
