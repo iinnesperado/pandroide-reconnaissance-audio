acf7fffabc25908314b804d67cd43ebf5999ab51
iinnesperado <inestianrp@gmail.com>
2025-03-14 17:26:02 +0100

    added comments to explain + a little change on the score calculation

--- audio_processing.py
+++ audio_processing.py
@@ -1,6 +1,5 @@
 from faster_whisper import WhisperModel
 import time
-import os
 import glob
 
 model_size = "large-v3"
@@ -12,14 +11,15 @@ model_size = "large-v3"
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 
-path = "/Users/ines/androide/p-androide/samples"
-
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
 timeFile = open("data/exec_time.txt", "w")
 scriptFile = open("data/fw_scripts.txt", "w")
 i = 1
 
-for name in glob.glob('samples/*.m4a'):
+directory = glob.glob('samples/*.m4a') + glob.glob('samples/*.mp3')
+
+for name in directory:
     start = time.time()
     segments, info = model.transcribe(name, beam_size=5)
     end = time.time()
@@ -34,6 +34,7 @@ for name in glob.glob('samples/*.m4a'):
     for segment in segments:
         scriptFile.write("%s" % (segment.text))
     
+    print("Faster-Whisper finit pour fichier ", name)
     i += 1
 
 timeFile.close()
--- give_audio_accuracy.py
+++ give_audio_accuracy.py
@@ -3,54 +3,59 @@ import collections
 import glob
 import re
 
+# Transcription faite par faster whisper de tous les fichiers audio
 fwFile = open("data/fw_scripts.txt", "r")
 fw_read = fwFile.read()
 
-ogFile = open("data/og_transcripts.txt", "r")
+# Transcription faites par nous utilisée comme référence de tous les fichier audio
+# Il est important que l'ordre des transcriptions faite à main soient
+# dans le même ordre que les transcriptions faites par faster-whisper
+ogFile = open("data/og_scripts.txt", "r")
 og_read = ogFile.read()
 
-samplesName = glob.glob("samples/*.m4a")
+directory = glob.glob('samples/*.m4a') + glob.glob('samples/*.mp3')
 
+# On transforme les textes en liste de mots
 fw_file = re.split(" |, |\t|\n", fw_read)
 og_file= re.split(" |, |\t|\n", og_read)
-#print(og_file[:200]
 
+# On store le score dans in dict('nom de fichier' : valeur du score dans [0,1])
 score = dict()
 
 i = 2
-for name in samplesName:
+for name in directory:
     try:
+        # On selectionne que la partie texte d'un seul fichier audio pour faire la 
+        # comparaison et donner un score
         fw_text = fw_file[fw_file.index(name)+1:fw_file.index('(%d)'%i)]
         og_text = og_file[og_file.index(name)+1:og_file.index('(%d)'%i)]
     except:
         fw_text = fw_file[fw_file.index(name)+1:]
         og_text = og_file[og_file.index(name)+1:]
-    total = len(og_text)
-    print("name file : ", name)
-    print("mots dans fw :",len(fw_text))
-    print("mots dans og :", total)
-    '''for word in fw_text:
-        if word in og_text:
-            cpt += 1
-    '''
+    total_mots = len(og_text)
+    # print("Nom du fichier : ", name)
+    # print("Nobre de mots dans fw :",len(fw_text))
+    # print("Nombre de mots dans og :", total_mots)
+    
+    # missedWords donne les mots qui ne sont pas dans fw_text par rapport à og_text
+    missedWords = set(og_text) - set(fw_text)
+    # print("Mots manquants dans la transcription de fw :", missedWords))
 
-    #cpt = total
+    # Transforme la liste de mots og_text en dictionaire de fréquences de mots
+    # pour savoir le nombre total de mots qui manquent (cas où un mots serait plusieurs fois
+    # mal compris)
+    og_dict = collections.Counter(og_text)
+    cpt = 0 
+    for word in missedWords:
+        cpt += og_dict[word]
     
-    fw = collections.Counter(fw_text)
-    og = collections.Counter(og_text)
-    print("this", (set(og) - set(fw)))
-    '''for word in fw:
-        if word in og:
-            cpt = cpt - max(fw[word] - og[word], 0)'''
-    cpt = total - len(set(og_text) - set(fw_text))
-    
-    score[name] = cpt/total
+    score[name] = (total_mots - cpt)/total_mots
     i += 1
 
 
 #print("score : ", score)
 
-scoreFile = open("data/accuracy.txt", "w")
+scoreFile = open("data/accuracy_score.txt", "w")
 scoreFile.write("# 1 - file name\n")
 scoreFile.write("# 2 - same word/nb total mots %\n")
 for sample in score:
